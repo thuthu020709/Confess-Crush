383f6c06eb738fb2f4d1673bcd88b39f0926a72d
thuthu020709 <90281158+thuthu020709@users.noreply.github.com>
2021-09-08 02:05:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    titleWeb: "Bấm vô đây nè",
+    introTitle: 'Anh ơi',
+    introDesc: `Đừng giận em nữa`,
+    btnIntro: '😔😔',
+    title: 'Em biết lỗi rồi mà',
+    desc: 'Anh để ý tới em đi 🥺🥺 ',
+    btnYes: 'Hết giận <3<3 ',
+    btnNo: 'Hông nha :((',
+    question: 'Em thương anh lắm lắm 🙆🙆 Anh có thương em hong dạ?',
+    btnReply: 'Gửi cho em đi <3',
+    reply: 'Thương thương thương',
+    mess: 'Anh mà hong hết giận là em dỗi ngược lại á',
+    messDesc: 'Giỡn thoi anh mà hong hết giận thì em năn nỉ tiếp :3 ',
+    btnAccept: 'Yêu anhh ♥️',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
